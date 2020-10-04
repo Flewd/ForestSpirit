013dee7eb1b397f3fc4b6a392551042c640abcc6
Flewd <me@mike-flood.com>
2020-10-03 21:28:26 -0500

    moving platform prefab and example

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,24 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _platformRigidBody;
+    [SerializeField] private Transform _targetPosition;
+
+    [SerializeField] private bool _loop = true;
+    [SerializeField] private float _duration = 1;
+    [SerializeField] private Ease _easeType = Ease.Linear;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int loopCount = (_loop ? -1 : 0);
+
+        TweenParams tweenParams = new TweenParams()
+            .SetLoops(loopCount, LoopType.Yoyo)
+            .SetEase(_easeType);
+
+        _platformRigidBody.DOMove(_targetPosition.position, _duration).SetAs(tweenParams);
+    }
+}
---
+++ Assets/MovingPlatformAttach.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformAttach : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rigidbody;
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Rigidbody>().velocity += _rigidbody.velocity;
+        }
+    }
+
+    /*
+    private void OnTriggerEnter(Collider other)
+    {
+        print("ENTER");
+        if(other.tag == "Player")
+        {
+            other.transform.parent = transform;
+        }   
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("EXIT");
+        if (other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+*/
+}
