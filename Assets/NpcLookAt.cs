599b04f8cfbce672e04102bc79c9900622fd4cf2
Flewd <me@mike-flood.com>
2020-10-04 00:11:18 -0500

    game scene setup and npc spirits

---
+++ Assets/NpcLookAt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class NpcLookAt : MonoBehaviour
+{
+
+    Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(new Vector3(
+            playerTransform.position.x, 
+            transform.position.y, 
+            playerTransform.position.z));
+    }
+}
---
+++ Assets/NpcParticleAnimationTrigger.cs
@@ -0,0 +1,56 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+
+public class NpcParticleAnimationTrigger : MonoBehaviour
+{
+    [SerializeField] SkinnedMeshRenderer _meshRenderer;
+
+    [SerializeField] ParticleSystem _particleSystem;
+    [SerializeField] Transform _targetParticlePosition;
+    [SerializeField] float _particleAnimationDuration = 3;
+    [SerializeField] private Ease _particleEaseType = Ease.InOutSine;
+
+    bool animationStarted = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (animationStarted == false && other.tag == "Player")
+        {
+            animationStarted = true;
+            StartCoroutine(PlaySpiritAnimation());
+        }
+    }
+
+    private IEnumerator PlaySpiritAnimation()
+    {
+        if (_targetParticlePosition != null)
+        {
+            TweenParams tweenParams = new TweenParams()
+                .SetEase(_particleEaseType);
+
+            _particleSystem.gameObject.SetActive(true);
+            _particleSystem.Play();
+
+            _particleSystem.transform.DOMove(_targetParticlePosition.position, _particleAnimationDuration).SetAs(tweenParams).OnComplete(() => StartCoroutine(CleanupParticles()));
+        }
+
+        Material mat = _meshRenderer.material;
+
+        while (mat.color.a > 0)
+        {
+            Color newColor = mat.color;
+            newColor.a -= Time.deltaTime;
+            mat.color = newColor;
+            _meshRenderer.material = mat;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private IEnumerator CleanupParticles()
+    {
+        _particleSystem.Stop();
+        yield return new WaitForSeconds(15);
+        _particleSystem.gameObject.SetActive(false);
+    }
+}
